bbc361a647f6f405df6e129f18e5872a462339f6
diamondsplash2023 <andresjoseruedatello@gmail.com>
2023-12-31 15:01:33 -0500

    C5

--- operations.py
+++ operations.py
@@ -1,7 +1,9 @@
 a = 234
 b = 33333
+c = 0.98
 
 print("operation 1 is", a + b)
 print("operation 2 is", a - b)
 print("operation 3 is", a * b)
 print("operation 4 is", a / b)
+print("a decimal number:", c)
