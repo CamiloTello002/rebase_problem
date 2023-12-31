e1d3958686d46f172edacd848a028e45a020938a
diamondsplash2023 <andresjoseruedatello@gmail.com>
2023-12-31 14:59:32 -0500

    C4

--- operations.py
+++ operations.py
@@ -6,4 +6,14 @@ print("operation 1 is", a + b)
 print("operation 2 is", a - b)
 print("operation 3 is", a * b)
 print("operation 4 is", a / b)
+
 print("a decimal number:", c)
+
+users = {
+        1: "remberto",
+        2: "julia",
+        3: "camilo",
+        4: "juliana",
+        5: "laura",
+        
+        }
