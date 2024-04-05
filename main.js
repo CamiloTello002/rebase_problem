cce0aa11ac94654b2201c38883bea3bd26c5ca4f
diamondsplash2023 <andresjoseruedatello@gmail.com>
2024-04-04 23:57:13 -0500

    feat(greetings): added greeting functions

---
+++ main.js
@@ -0,0 +1,10 @@
+function greetMom(name){
+  console.log('hello daddy, mr', name)
+}
+
+function greetDad(name){
+  console.log('hello uncle, mr', name)
+}
+
+greetMom('RoseMary')
+greetDad('Jamin')
