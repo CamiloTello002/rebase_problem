6a435d6db33d0804f3fbf32a26f3c212287e7be1
diamondsplash2023 <andresjoseruedatello@gmail.com>
2024-04-05 00:04:35 -0500

    fixed bugs in functions

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function greetMom(name){
-  console.log('hello daddy, mr', name)
+  console.log('hello mommy, mr', name)
 }
 
 function greetDad(name){
-  console.log('hello uncle, mr', name)
+  console.log('hello daddy, mr', name)
 }
 
 greetMom('RoseMary')
